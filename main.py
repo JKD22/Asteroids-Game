86c2cc9fbea26b08dc6b6fc4a0610a77d41425c4
JKD22 <joshuaskraft@gmail.com>
2025-03-22 12:30:52 -0400

    added fps, circleshape and player

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ import pygame
 from constants import *
 
 def main():
+    #initialize pygame
     pygame.init()
+    #set up the clock
+    clock = pygame.time.Clock()
+    #set game time to 0
+    dt = 0
+    #set up the screen  
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH} Screen height: {SCREEN_HEIGHT}")
+    #game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(BLACK)
-
+        #updates display
+        pygame.display.flip()
+        #updates clock
+        dt = clock.tick(60) / 1000
+        fps = clock.get_fps()
+        print(f"FPS: {fps}")
 if __name__ == "__main__":
     main()
